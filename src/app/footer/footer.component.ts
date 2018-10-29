23839fbb6ccd4a4794cb1b8c0125f7f1937e1871
ray1295 <ralake95@gmail.com>
2018-10-29 12:08:16 +0000

    Footer - 2 [Top footer]

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
  selector: 'app-footer',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class FooterComponent implements OnInit {
+
   constructor() { }
 
   ngOnInit() {
