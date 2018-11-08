1aa604f72dace4198af8c6cb004db9795d3c4279
ray1295 <ralake95@gmail.com>
2018-11-08 13:41:49 +0000

    Register - 3 [Course*]

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -7,7 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor() { }
+  allowAdditem = false;
+  itemAddStatus = "No item was added";
+  checkout:string = "Checkout out now!";
+  itemName:string = "Shirt XL";
+
+  // Method -> Returning a string
+  getCheckout() {
+    return this.checkout;
+  }
+
+  // Method -> 'On' identifies that it's triggered within template.
+  onAddItem() {
+    this.itemAddStatus = "Item " + this.itemName + "was added";
+  }
+
+  //Method -> Fetching the data in the input event.
+  onUpdateItemList(event: Event) {
+    // HTMLInputElement -> Informing TS about the type of html element will be of input element.
+    this.itemName = (<HTMLInputElement>event.target).value;
+  }
+
+  constructor() {
+    // Method -> Running function after given time. 
+    setTimeout(() => {
+      this.allowAdditem = true;
+    }, 2000)
+  }
 
   ngOnInit() {
   }
