19e6a592c3e50ee323f1b3b51724245a3bcd5480
ray1295 <ralake95@gmail.com>
2018-11-09 11:52:13 +0000

    Course - 2 [Section 2, Directives]

--- src/app/pages/wishlist/wishlist.component.ts
+++ src/app/pages/wishlist/wishlist.component.ts
@@ -9,12 +9,20 @@ export class WishlistComponent implements OnInit {
   
   itemWish:string = '';
   addWish:boolean = false;
+  wishStatus: string = 'empty';
+  items = ['Shoes', 'Shirt'];
 
   // Built in method -> called once component is created
   constructor() { 
-    this.itemWish = Math.random() > 0.5 ? 'online' : 'offline'
+    this.wishStatus = Math.random() > 0.5 ? 'item' : 'empty';
   }
 
+  // Method -> Color depeneding on wishStatus
+  getColor() {
+    return this.wishStatus === 'item' ? 'green' : 'red';
+  }
+
+  // Method -> 
   onAddWish() {
     if (this.itemWish !== '')
       return true;
