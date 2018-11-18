298c88a75a0f0a7cad5d9d3bc09dee3593b896fe
ray1295 <ralake95@gmail.com>
2018-11-18 10:53:24 +0000

    error

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-admin',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
+  
+  // Properties -> Properties inside a component are only accessible inside that component.
+  // Decorator: @Input() -> Gives property outside visibility and usage -> outside component binding to property selector can now access it.
+  // Alias @Input('srvElement') -> Passing arg with property name as you'd want to have it accesssed from the outside.
+  @Input('srvElement') element: {type: string, name: string, content: string};
 
   constructor() { }
 
